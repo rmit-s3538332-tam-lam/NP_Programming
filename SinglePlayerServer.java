160d9507dc0f3497bdbd27539fe86ac1e5ab1d12
Tam Lam <tamlam@Tams-MacBook.local>
2018-05-04 15:45:11 +1000

    Generate secret code base on x

--- SinglePlayerServer.java
+++ SinglePlayerServer.java
@@ -4,20 +4,27 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SinglePlayerServer {
     static final int PORT_NUMBER = 1324;
     public static void main(String[] args) {
+        int x;
         // ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
         // Socket s = serverSocket.accept();
-        System.out.println("Please enter an integer between 3 - 8:");
-        getX();
+        // System.out.println("Please enter an integer between 3 - 8:");
+        x = getX();
+        int[] secretCode = generateSecretCode(x);
+        System.out.println(Arrays.toString(secretCode));
+ 
         
     }
     public static int getX(){
         int x  = 0;
         while(true){
             try{
+                System.out.println("Please enter an integer between 3 - 8:");
                 InputStream inStream = System.in;
                 BufferedReader  reader =  new BufferedReader(new InputStreamReader(inStream));
                 String line = null;
@@ -30,12 +37,7 @@ public class SinglePlayerServer {
                             System.out.println("Selected x: "+  x);
                             break;
                         }
-                    }else{
-                        System.out.println("Please enter an integer between 3 - 8:");
                     }
-                  
-                } else{
-                    System.out.println("Please enter an integer between 3 - 8:");
                 }
             } catch (IOException  e){
                 e.printStackTrace();
@@ -43,8 +45,25 @@ public class SinglePlayerServer {
         }
         return x;
     }
+
+    private static int[] generateSecretCode(int size){
+        
+
+        ArrayList<Integer> list = new ArrayList<>(11);
+        for (int i = 0; i <= 10; i++){
+            list.add(i);
+        }
+        int[] secretCode = new int[size];
+        for (int count = 0; count < size; count++){
+            secretCode[count] = list.remove((int)(Math.random() * list.size()));
+        }
+        return secretCode;
+    }
     private static void quittingOnX(String line){
-        if(line.equalsIgnoreCase("x")) System.exit(0);
+        if(line.equalsIgnoreCase("x")) {
+            System.out.println("Quitting...");
+            System.exit(0);
+        }
     }
     private static int convertStringToInt(String valueString) throws NumberFormatException {
         return Integer.parseInt(valueString);
