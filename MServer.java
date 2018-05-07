f6babcb9012f89785ed88797a3d89a273994c3e1
Tam Lam <tamlam@Tams-MacBook.local>
2018-05-07 14:57:12 +1000

    Refactor getPlayerName

--- MServer.java
+++ MServer.java
@@ -12,75 +12,89 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
-public class MServer  extends SocketAgent{
-    private static HashSet<String> playerNames = new HashSet<String>();
-    
 
-    public static void main(String[] args) throws IOException{
+public class MServer extends SocketAgent {
+    private static HashSet<String> playerNames = new HashSet<String>();
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Server is running...");
-        ServerSocket serverSocket  = new ServerSocket(PORT_NUMBER);
+        ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
         BufferedReader consoleIn = new BufferedReader(new InputStreamReader(System.in));
-        QuitThread quitThread= new QuitThread();
+        QuitThread quitThread = new QuitThread();
         quitThread.start();
         String line;
-        try{
-            while(true){
+        try {
+            while (true) {
                 new ClientThread(serverSocket.accept()).start();
             }
-        } finally{
+        } finally {
             serverSocket.close();
         }
 
-
     }
 
-    public static class ClientThread extends Thread{
-        private  String playerName;
-        private  BufferedReader in;
-        private  PrintWriter  out;
+    public static class ClientThread extends Thread {
+        private String playerName;
+        private BufferedReader in;
+        private PrintWriter out;
         private Socket s;
-        public ClientThread(Socket s){
-           
+
+        public ClientThread(Socket s) {
+
             this.s = s;
         }
+
         @Override
-        public void run(){
-            try{
+        public void run() {
+            try {
                 System.out.println("New client is connected...");
                 in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                out = new PrintWriter(s.getOutputStream(),true);
-                
-                //getting player name
-                while(true){
-                    out.println(SUBMIT_NAME);
-                    System.out.println("Getting player  name...");
-                    playerName = in.readLine();
-                    if(playerName == null){return;}
-                    synchronized(playerNames){
-                        if(!playerNames.contains(playerName)){
-                            playerNames.add(playerName);
-                            break;
-                        }
-                    }
-                }
-                out.println(NAME_ACCEPTED);
-                System.out.println("Player  name:"+ playerName);
+                out = new PrintWriter(s.getOutputStream(), true);
 
+                // getting player name and add it to playerQueue
+                String playerName = getPlayerNameFromClient(s);
+                addPlayerToQueue(playerName);
+                
                 //
-            }catch (IOException e){
+
+            } catch (IOException e) {
                 e.printStackTrace();
-            }  finally{
-                // if(playerName!= null){
-                //     playerNames.remove(playerName);
-                // }
-                try{
+            } finally {
+                if (playerName != null) {
+                    playerNames.remove(playerName);
+                }
+                try {
                     s.close();
-                }catch(IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
 
-        }   
-       
+        }
+
+        public String getPlayerNameFromClient(Socket s) throws IOException {
+            String playerName;
+            while (true) {
+                out.println(SUBMIT_NAME);
+                System.out.println("Getting player  name...");
+                playerName = in.readLine();
+                if (playerName == null) {
+                    return playerName;
+                }
+                if (!playerNames.contains(playerName)) {
+                    break;
+
+                }
+            }
+            out.println(NAME_ACCEPTED);
+            System.out.println("Player  name:" + playerName);
+            return playerName;
+        }
+
+        public void addPlayerToQueue(String playerName) {
+            synchronized (playerNames) {
+                playerNames.add(playerName);
+            }
+        }
     }
 }
